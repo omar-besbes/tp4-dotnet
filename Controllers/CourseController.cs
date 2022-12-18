118db48fc357f82eaa85722634067f99286d0ecc
Omar Besbes <omar.besbes@insat.ucar.tn>
2022-12-18 23:51:51 +0100

    feat: courses view

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tp4_dotnet.Controllers;
+
+[Route("Courses")]
+public class CourseController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
