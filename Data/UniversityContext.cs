b950f566e2908137b14b7c58dea3afb9fe0db9ec
Omar Besbes <omar.besbes@insat.ucar.tn>
2022-12-18 22:48:34 +0100

    feat: database context

---
+++ Data/UniversityContext.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+using tp4_dotnet.Models;
+
+namespace tp4_dotnet.Data;
+
+public class UniversityContext : DbContext
+{
+    public DbSet<Student> Student { get; set; }
+
+    private static UniversityContext Context;
+
+    private UniversityContext(DbContextOptions o) : base(o)
+    {
+    }
+
+    public UniversityContext Instantiate_UniversityContext(IConfiguration _configuration)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<UniversityContext>();
+
+        optionsBuilder.UseSqlite(_configuration.GetConnectionString("SQLite"));
+
+        if (Context == null) Context = new UniversityContext(optionsBuilder.Options);
+
+        return Context;
+    }
+}
